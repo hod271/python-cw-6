9b4c951e624460b60262188df89a9f760b846cd3
hod271 <h5j4r271@gmail.com>
2023-09-12 20:49:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-# write your code here
+#الجزء الاول
+
+person = {
+    'name': 'hajar',
+    'age': 15,
+    'hobbies': ['reading' , 'swimming']
+    }
+
+print(person['name'])
+print(person['age'])
+
+#الجزء الثاني
+person['age'] = '16'
+person['country'] = 'kuwait'
+print(person)
+print(len(person))
+
+
+
+        
